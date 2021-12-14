7f12e605c7bccdb200dd4619f4aec4e7ed6fc8f6
abe-sinjiro <real_striver@outlook.com>
2021-12-14 18:15:05 +0900

    update with latest libraries and interfaces

--- contracts/interface/IOwnable.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.8.7;
-
-interface IOwnable {
-    function owner() external view returns(address);
-
-    function renounceOwnership() external;
-
-    function transferOwnership(address newOwner_) external;
-}
